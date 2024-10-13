b2a8b3f0a5eea01ec546883a9e4020360ea878de
lucas <lukscabral95@gmail.com>
2024-10-13 20:32:50 -0300

    exercicios 06-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c =(f-32)*(5/9) 
+  c = c.toFixed(1);
+  return Number.parseFloat(c);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c*(9/5)+32;
+  f = f.toFixed(1);
+  return Number.parseFloat(f);
 };
 
 // Do not edit below this line
@@ -9,3 +15,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
