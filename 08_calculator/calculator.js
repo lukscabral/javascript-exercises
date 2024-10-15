9a6c6b7eff56bd624a254d6051020a9d452c7c37
lucas <lukscabral95@gmail.com>
2024-10-15 17:55:36 -0300

    exercicio 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+  let value = a + b;
+	return value
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	 let value = a - b;
+   return value
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  
+  let value = 0;
+	for(let num of array){
+    value += num;
+  }return value
 };
 
-const multiply = function() {
-
+const multiply = function(array) {// ver o resultado dessa inicializaçao com 1
+  let value = 1;
+  for(num of array){
+    value *= num;
+  }return value
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+	let value = 1;
+  if(factorial === 0 || factorial === 1){
+    return value
+  } else {
+    for(i = 1; i<=factorial; i++){
+      value *= i; 
+    }return value
+  }
 };
 
 // Do not edit below this line
