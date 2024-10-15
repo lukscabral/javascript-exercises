df0da08588428a6190fe9c4bbe0954b7c87e2c4b
lucas <lukscabral95@gmail.com>
2024-10-15 20:33:46 -0300

    exercicio 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (arg) {
+    let text_a = arg.replace(/[^\w]/g, "");//remove pontuaçoes
+    text_a = text_a.toLowerCase(); //transforma td em minuscula
+    let text_b = '';
+    text_b = text_a.split("");//array de letras individuais
+    text_b = text_b.reverse().join("");//reverte e junta novamente
+    if(text_b == text_a){
+        return true
+    }else return false
+    
 };
-
+//funciona pq ingles tem pontuaçoes diferentes
 // Do not edit below this line
 module.exports = palindromes;
