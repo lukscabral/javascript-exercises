6ae38e6580c78098032963b64a5ddd8908748219
lucas <lukscabral95@gmail.com>
2024-10-15 21:55:16 -0300

    exercicio 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    arg = arg.toString();
+
+    if(arg == 0){
+        return 0
+    } else if (arg < 0){
+        return "OOPS"
+    }else {
+        const x = [1,1];
+        fib = arg - 1;
+        let value =0;
+        for(i=2;i<=arg; i++){
+            value = x[i-1]+x[i-2];
+        x.push(value)
+        }return x[fib]
+}
 
 };
 
