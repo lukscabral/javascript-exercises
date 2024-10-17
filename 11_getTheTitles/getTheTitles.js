a6d8c2f206c33dd312d2e787f220c6bacf11ada0
lucas <lukscabral95@gmail.com>
2024-10-17 18:15:59 -0300

    exercicio 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
-const getTheTitles = function() {
 
-};
+const getTheTitles = function(arg) {
+    const value = arg.map((x) => x.title)
+    return value
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+/*
+    let colecao = 0;
+    colecao = Object.values(x);//trouxe os objetos 
+    value = [colecao[0].title, colecao[1].title];//fez um array na mao dos title de cada objeto dentro do "books"
+    return value
+=============================================================
+let colecao = 0;
+    colecao = Object.values(x);//trouxe os objetos 
+    let temp = [];
+    let value =[];
+  
+
+    for(let i= 0;i<colecao.length; i++){
+        temp = colecao[i].title;
+        value.push(temp);
+    }
+    return value
+}
+*/
